cfb6383a3b5edcc9411209ba015f94820f0b2388
Ubuntu <ubuntu@ip-172-31-14-188.us-east-2.compute.internal>
2019-03-07 15:10:39 +0000

    updated setup_es.sh

--- setup_es.sh
+++ setup_es.sh
@@ -10,6 +10,12 @@ sysctl -w vm.max_map_count=262144
 
 export LOGSTASH_KEYSTORE_PASS=pwgeheim
 
+mkdir es
+
+git clone https://github.com/elastic/stack-docker.git es
+
+cd es
+
 docker-compose -f setup.yml up
 
 docker-compose up -d
